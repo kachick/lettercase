913bef74fda0db66410a4b5205a5b490397e6bf5
Kenichi Kamiya <kachick1@gmail.com>
2012-09-14 06:39:58 +0900

    add YARD

--- lib/lettercase/constants.rb
+++ lib/lettercase/constants.rb
@@ -1,5 +1,6 @@
 module LetterCase
 
+  # @return [String]
   DELIMITER = '_'.freeze
   private_constant :DELIMITER if respond_to? :private_constant
 
--- lib/lettercase/stringextension.rb
+++ lib/lettercase/stringextension.rb
@@ -2,26 +2,31 @@ module LetterCase
 
   module StringExtension
 
+    # @return [String]
     def UPCASE
       upcase
     end
 
+    # @return [String]
     def snakecase
       gsub(/([a-z])([A-Z])/) {[$1, $2].join DELIMITER}.downcase
     end
     
     alias_method :snake_case, :snakecase
     
+    # @return [String]
     def force_pascalcase
       split(DELIMITER).map(&:capitalize).join
     end
 
+    # @return [String]
     def pascalcase
       gsub(/\w+/) {|s|s.extend(StringExtension).force_pascalcase}
     end
     
     alias_method :PascalCase, :pascalcase
     
+    # @return [String]
     def camelcase
       gsub(/\w+/) {|chunk|
         ''.tap {|s|
